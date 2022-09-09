31ab7c18456bd78cf6c820d83cd36153504b4519
Xing <820239854@qq.com>
2022-09-09 12:23:56 +0800

    Update win32_handmade.cpp
    PatBlt (“Pat Blit”)  a super simple function, allowing us to fill out a rectangle with a specified color or pattern

--- src/win32_handmade.cpp
+++ src/win32_handmade.cpp
@@ -1,11 +1,98 @@
 #include <windows.h>
 
-int CALLBACK
-WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR     lpCmdLine,
-	int       nShowCmd)
+LRESULT CALLBACK MainWindowCallback(HWND Window,
+                                    UINT Message,
+                                    WPARAM WParam,
+                                    LPARAM LParam)
 {
-	MessageBox(0, "This is Handmade Hero.", "Handmade Hero", MB_ICONINFORMATION | MB_OK);
+	LRESULT Result = 0;
+	switch (Message)
+	{
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+		} break;
+
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+		} break;
+    
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WM_CLOSE\n");
+		} break;
+    
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			PatBlt(DeviceContext, X, Y, Width, Height, BLACKNESS);
+
+			EndPaint(Window, &Paint);
+		} break;
+
+		default:
+		{
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
+	}
+	return (Result);
+}
+
+
+int CALLBACK WinMain(HINSTANCE Instance,
+                     HINSTANCE PrevInstance,
+                     LPSTR     CommandeLine,
+                     int       ShowCode)
+{
+	WNDCLASSA WindowClass = {};
+	WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	// WindowClass.hIcon;
+	WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+	if (RegisterClassA(&WindowClass))
+	{
+		HWND Window = CreateWindowExA(0, WindowClass.lpszClassName, "Handmade Hero",
+		                              WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		                              CW_USEDEFAULT, CW_USEDEFAULT,
+		                              CW_USEDEFAULT, CW_USEDEFAULT,
+		                              0, 0, Instance, 0);
+		if (Window)
+		{
+			MSG Message;
+			for(;;)                              // a for loop which would run forever
+			{
+				MSG Message;
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if (MessageResult > 0)          // 0 is the WM_QUIT message, -1 is invalid window handle
+				{
+					TranslateMessage(&Message);
+					DispatchMessageA(&Message);
+				}
+				else
+				{
+					break;                      // break out of the loop
+				}
+			}
+		}
+		else
+		{
+			// Window Creation failed! 
+			// TODO(casey): Logging
+		}
+	}
 	return (0);
 }
+
