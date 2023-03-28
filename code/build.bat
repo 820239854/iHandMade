8286486b0ed14b672759a4522039ee5f8d9eede2
Xing <820239854@qq.com>
2023-03-28 19:51:44 +0800

    Animating a Back Buffer

--- code/build.bat
+++ code/build.bat
@@ -3,5 +3,5 @@
 mkdir .\build
 pushd .\build
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-cl -Zi ..\code\win32_handmade.cpp user32.lib gdi32.lib
+cl -FC -Zi ..\code\win32_handmade.cpp user32.lib gdi32.lib
 popd
